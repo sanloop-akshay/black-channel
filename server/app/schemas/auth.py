40cc6eb58026781fc6996667240f4bf2eb1fa144
sanloop-akshay <akshaypranavb@gmail.com>
2025-09-19 21:59:25 +0530

    Remove : Design Fix Schema

--- server/app/schemas/auth.py
+++ server/app/schemas/auth.py
@@ -4,18 +4,9 @@ from typing import Optional
 from uuid import UUID
 from datetime import datetime
 
-class TokenResponse(BaseModel):
-    access_token: str
-    token_type: str = "bearer"
 
 class LoginRequest(BaseModel):
     username: constr(strip_whitespace=True, min_length=1)
     password: constr(strip_whitespace=True, min_length=1)
 
-class UserOut(BaseModel):
-    id: UUID
-    username: str
-    last_login: Optional[datetime]
 
-    class Config:
-        orm_mode = True
