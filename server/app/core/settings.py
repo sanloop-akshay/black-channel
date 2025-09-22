f2ec81efbe60e0261e723ed37774c024fdb5ff92
sanloop-akshay <akshaypranavb@gmail.com>
2025-09-22 18:51:30 +0530

    Intiate : Mailer Task and Update Settings

--- server/app/core/settings.py
+++ server/app/core/settings.py
@@ -1,5 +1,7 @@
-import redis
+# app/core/settings.py or wherever you keep settings
+from celery import Celery
 from app.core.config import settings
+import redis
 
 redis_client = redis.Redis(
     host=settings.REDIS_HOST,
@@ -9,4 +11,14 @@ redis_client = redis.Redis(
     decode_responses=True
 )
 
-storage_uri=f"redis://{redis_client.connection_pool.connection_kwargs['host']}:{redis_client.connection_pool.connection_kwargs['port']}/{redis_client.connection_pool.connection_kwargs['db']}"
+storage_uri = f"redis://{redis_client.connection_pool.connection_kwargs['host']}:{redis_client.connection_pool.connection_kwargs['port']}/{redis_client.connection_pool.connection_kwargs['db']}"
+
+celery_app = Celery(
+    "black_channel",
+    broker=storage_uri,
+    backend=storage_uri
+)
+
+celery_app.conf.task_routes = {
+    "tasks.mailer_task.*": {"queue": "mail_queue"},
+}
