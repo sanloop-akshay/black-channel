8a5c823e1ef8074f67338b536ef49ad8ec6db297
sanloop-akshay <akshaypranavb@gmail.com>
2025-09-22 18:45:48 +0530

    Init : Signup with redis server

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -2,15 +2,15 @@ from pydantic import Field
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-
     DATABASE_URL: str = Field(..., env="DATABASE_URL")
+    
     SECRET_KEY: str = Field(..., env="SECRET_KEY")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
-    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7 
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
     ACCESS_COOKIE_NAME: str = "access_token"
     REFRESH_COOKIE_NAME: str = "refresh_token"
-    COOKIE_SECURE: bool = False  
+    COOKIE_SECURE: bool = False
     COOKIE_SAMESITE: str = "lax"
 
     REDIS_HOST: str = Field("localhost", env="REDIS_HOST")
@@ -18,8 +18,13 @@ class Settings(BaseSettings):
     REDIS_DB: int = Field(0, env="REDIS_DB")
     REDIS_PASSWORD: str = Field("", env="REDIS_PASSWORD")
 
+    EMAIL_HOST: str = Field(..., env="EMAIL_HOST")
+    EMAIL_RECEIVER: str = Field(..., env="EMAIL_RECEIVER")
+    APP_PASSWORD: str = Field(..., env="APP_PASSWORD")
+
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+
 
 settings = Settings()
