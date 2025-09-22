4f6a31225e4369cf02b9996bc73d527c37ec5cc3
sanloop-akshay <akshaypranavb@gmail.com>
2025-09-22 22:54:13 +0530

    Init : Cryptography for Secure encryption/decryption Messaging between linux and Kivy APK

--- server/app/core/security.py
+++ server/app/core/security.py
@@ -4,6 +4,11 @@ from passlib.context import CryptContext
 from jose import jwt
 from app.core.config import settings
 from app.core.settings import redis_client
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+import os
+import base64
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -41,3 +46,26 @@ def blacklist_token(token: str, expire_seconds: int):
 
 def is_token_blacklisted(token: str) -> bool:
     return redis_client.exists(token) > 0
+
+
+#Cryptography for Web Socket Security (Now no Redis Support)
+def generate_key() -> bytes:
+    return os.urandom(32)
+
+def encrypt_message(key: bytes, plaintext: str) -> str:
+    iv = os.urandom(16)
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(plaintext.encode()) + padder.finalize()
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    ct = encryptor.update(padded_data) + encryptor.finalize()
+    return base64.b64encode(iv + ct).decode()
+
+def decrypt_message(key: bytes, ciphertext: str) -> str:
+    raw = base64.b64decode(ciphertext)
+    iv, ct = raw[:16], raw[16:]
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    padded_data = decryptor.update(ct) + decryptor.finalize()
+    unpadder = padding.PKCS7(128).unpadder()
+    return (unpadder.update(padded_data) + unpadder.finalize()).decode()
